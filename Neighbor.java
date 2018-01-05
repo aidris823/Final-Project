b03f06d6eaa379b59cb3787a37e4ea9a00ba93dd
aidris823 <pokeanand@gmail.com>
2018-01-05 14:11:48 -0500

    Checking commits with my wonderful Mr. Rogers file.

---
+++ Neighbor.java
@@ -0,0 +1,5 @@
+public class Neighbor{
+	public static void main(String[] arguments){
+		System.out.println("It's a beautiful day in this neighborhood - a beautiful day for a neighbor.  Would you be mine?  Would you be mine?");
+	}
+}
