69e7e7ce3325d70ad7f34231a0b4507fcdc6aca4
aidris823 <pokeanand@gmail.com>
2018-01-09 16:47:31 -0500

    Updating Piece

---
+++ Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn extends Piece{
+    private boolean hasMoved;
+    public Pawn(){
+	hasMoved = false;
+    }
+    //
+    public void move(){
+    }
+    public void capture(){
+    }
+   
--- Piece.java
+++ Piece.java
@@ -2,6 +2,8 @@ public abstract class Piece {
 
     private int xcor;
     private int ycor;
+    private boolean isWhite;
+    //If isWhite is false, of course, then it's black.
     private int[] coords;
 
     public Piece(int xcor, int ycor) {
@@ -22,4 +24,9 @@ public abstract class Piece {
     }
     
     public abstract void move();
+    /* CSDOJO NOTES:
+Probably can't have move method because it doesn't say which square I can go to.
+Have a legal moves */
+    public abstract Square[] getLegalMoves();+
+    public abstract void capture();
 }
