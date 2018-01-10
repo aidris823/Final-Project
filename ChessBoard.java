ce331cc43ab36325c52a7a461e97fc9477d28504
Steven <schen36@stuy.edu>
2018-01-10 00:00:13 -0500

    Anand -- Adding Chessboard

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,36 +1,68 @@
-// First, make a working board on the Terminal.
+import java.util.Arrays;
 public class ChessBoard{
-    // Background colors:
-    private static final int black = 40;
-    private static final int white = 107;
-    //Array of squares:
-    private Square[][] board;
-    //Set up an eight by eight grid:
-    public ChessBoard(){
-	board = Square[8][8];
+  //Square colors:
+  private static final int black = 40;
+  private static final int white = 107;
+  
+  //Array of the "Square" class:
+  private Square[][] board;
+  
+  
+  
+  //Constructor that sets up board:
+  public ChessBoard(){
+    //Makes an eight by eight grid with "coordinates," per se.
+    board = new Square[8][8];
+    //Gives each square a color.
+    setBoardColor();
+  }
+  //Gives each Square in the board a color in an alternating (chessboard-style) fashion.
+  public void setBoardColor(){
+    //[Even, Even] coordinates are white.
+    for (int i = 0; i <= 6; i += 2){
+      for (int j = 0; j <= 6; j += 2){
+        (board[i][j]).setColor(white);
+      }
     }
-    //Sets up the board:
-    public void setBoard(){
-	//This double for loop is meant set up color values for each of the
-	// square values.
-	for (int i = 0; i < 8; i += 2){
-	    for (int j = 0; j < 8; j += 2){
-		(board[i][j]).setColor(white);
-	    }
-	}
-	for (int i = 1; i <= 7; i += 2){
-	    for (int j = 0; j < 8; j += 2){
-		(board[i][j]).setColor(black);
-	    }
-	}
+    //[Odd, Odd] coordinates are white.
+    for (int i = 1; i <= 7; i += 2){
+      for (int j = 1; j <= 7; j+ = 2){
+        (board[i][j]).setColor(white);
+      }
     }
+    //[Even, Odd] coordinates are black.
+    for (int i = 0; i <= 6; i += 2){
+      for (int j = 1; j <= 7; j+= 2){
+        (board[i][j]).setColor(black);
+      }
+    }
+    //[Odd, Even] coordinates are black.
+    for (int i = 1; i <= 7; i += 2){
+      for (int j = 0; j <= 7; j += 2){
+        (board[i][j].setColor(black));
+      }
+    }
+  }
+  public static void main(String[] arguments){
+    //For now, I am testing to see if the grid was set up properly through this main function.
     
-    public static void main(String[] arguments){
-	setBoard();
-	for (int i = 0; i < 8; i++){
-	    for (int j = 0; j < 8; j++){
-		System.out.println((board[i][j].getColor()));
-	    }
-	}
+    //Make an 8x8 2D Array of Strings: 
+    public String[][] testBoard;
+    testBoard = new String[8][8];
+    
+    //Give each spot a "B" if the Square at that coordinate has a color value equal to 40, "W" if it is 107:
+    for (int i = 0; i <= 7; i++){
+      for (int j = 0; j <= 7; j++){
+        if ((board[i][j]).getColor() == 40){
+          testBoard[i][j] = "B";
+        }
+        if ((board[i][j]).getColor() == 107){
+          testBoard[i][j] = "W";
+        }
+      }
     }
+          
+    //Print the array:
+    System.out.println(Arrays.deepToString(testBoard));
+  }
 }
