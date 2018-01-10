274246df1ba4c63d65fb628a6db3494790ff0206
Steven <schen36@stuy.edu>
2018-01-10 00:01:06 -0500

    Anand -- Adding Piece

--- Piece.java
+++ Piece.java
@@ -1,32 +1,30 @@
-public abstract class Piece {
+public abstract class Piece{
+  private int xcor;
+  private int ycor;
+  private boolean isWhite;
+  //If isWhite is false, of course, then it's black.
+  private int[] coords;
 
-    private int xcor;
-    private int ycor;
-    private boolean isWhite;
-    //If isWhite is false, of course, then it's black.
-    private int[] coords;
-
-    public Piece(int xcor, int ycor) {
+  public Piece(int xcor, int ycor) {
 	this.xcor = xcor;
 	this.ycor = ycor;
     }
-    
-    public int[] getCoords() {
-	return coords;
-    }
+  public int[] getCoords() {
+    return coords;
+  }
+  public int getX() {
+    return xcor;
+  }
 
-    public int getX() {
-	return xcor;
+  public int getY() {
+    return ycor;
+  }
+  //Returns an array of legal moves that a piece has.
+  public abstract Square[] getLegalMoves();
+  public boolean isInsideBounds(int row, int col){
+    if (row < 0 || row > 7 || col > 0 || col < 7){
+      return false;
     }
-
-    public int getY() {
-	return ycor;
-    }
-    
-    public abstract void move();
-    /* CSDOJO NOTES:
-Probably can't have move method because it doesn't say which square I can go to.
-Have a legal moves */
-    public abstract Square[] getLegalMoves();
-    public abstract void capture();
+    return true;
+  }
 }
