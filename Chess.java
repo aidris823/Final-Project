561225a84bcdf3575e2b9b6f59ddfd3c4d70ec4e
Steven <schen36@stuy.edu>
2018-01-10 22:04:04 -0500

    minor fixes

--- Chess.java
+++ Chess.java
@@ -7,7 +7,9 @@ public class Chess {
 	do {
 	    System.out.println("Please enter a move (e.g. \"b1 to c3\"): ");
 	    s = in.nextLine();
+	    makeMove(s.substring(0,2), s.substring(6);
 	} while( s.length() != 8 );
+	
 	return s;
     }
     
