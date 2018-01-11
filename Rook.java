e5d54cdc96f0eae0699644136d94db8761c09a42
StevenRamen <schen36@stuy.edu>
2018-01-11 14:26:24 -0500

    new

--- Rook.java
+++ Rook.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Rook extends Piece{
 
   private ChessBoard board = new ChessBoard();
@@ -93,36 +92,4 @@ public class Rook extends Piece{
     }
     return ans;
   } 
-=======
-public class Rook extends Piece {
-
-    public boolean checkMove(Location toSpot) {
-	if (toSpot.getRow() == getLoc().getRow()
-	    || toSpot.getCol() == getLoc().getCol()) {
-
-	    return true;
-	}
-	return false;
-    }
-    
-    public boolean makeMove(Location toSpot){
-	return null;
-    }
-
-    public Location[] mustBeOpen(Location toSpot) {
-	return null;
-    }
-
-    public String toString() {
-	if (color == Piece.WHITE) {
-	    return "\u265C";
-	}
-	else {
-	    return "\u2656";
-	}
-	// black chess rook	\u2656
-	// white chess rook	\u265C
-    }
-    
->>>>>>> Steven
 }
