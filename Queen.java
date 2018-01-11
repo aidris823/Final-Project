4fcb2facaffadd19fa39e9e3c7fd8c2beacc878e
StevenRamen <schen36@stuy.edu>
2018-01-11 14:25:31 -0500

    new

--- Queen.java
+++ Queen.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class Queen extends Piece{
 
   private ChessBoard board = new ChessBoard();
@@ -183,33 +183,3 @@ public class Queen extends Piece{
     return ans;
   }
 }
-  
-    
-=======
-public class Queen extends Piece {
-
-    public  boolean checkMove(Location toSpot) {
-	return null;
-    }
-    
-    public  boolean makeMove(Location toSpot){
-	return null;
-    }
-
-    public  Location[] mustBeOpen(Location toSpot) {
-	return null;
-    }
-
-    public  String toString() {
-	if (color == Piece.WHITE) {
-	    return "\u265B";
-	}
-	else {
-	    return "\u2655";
-	}
-	// black chess queen	\u2655
-	// white chess queen	\u265B
-    }
-    
-}
->>>>>>> Steven
