6f7f8f3c781b62bbe445d234f10985a828a6a5ae
Steven <schen36@stuy.edu>
2018-01-11 21:30:59 -0500

    added new piece - more simple

--- Piece.java
+++ Piece.java
@@ -1,30 +1,14 @@
-public abstract class Piece{
-  private int xcor;
-  private int ycor;
-  private boolean isWhite;
-  //If isWhite is false, of course, then it's black.
-  private int[] coords;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
-  public Piece(int xcor, int ycor) {
-	this.xcor = xcor;
-	this.ycor = ycor;
-    }
-  public int[] getCoords() {
-    return coords;
-  }
-  public int getX() {
-    return xcor;
-  }
-
-  public int getY() {
-    return ycor;
-  }
-  //Returns an array of legal moves that a piece has.
-  public abstract Square[] getLegalMoves();
-  public boolean isInsideBounds(int row, int col){
-    if (row < 0 || row > 7 || col > 0 || col < 7){
-      return false;
-    }
-    return true;
-  }
+@SuppressWarnings("serial")
+public abstract class Piece extends JLabel
+{
+	
+	public String color = "white";
+	public ImageIcon imageIcon;
+	public String pieceType;
+	
+	public abstract String getColor();
+	public abstract String getPieceType();
 }
