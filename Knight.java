6a4a977745817323eb67b3ab4cb063aad66c58a4
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-01-15 17:27:29 -0500

    Fixed counter issues

--- Knight.java
+++ Knight.java
@@ -1,50 +1,53 @@
-
 public class Knight extends Piece{
-  //Determines if a Pawn can move 2 squares or not.
-  private boolean hasMoved = false;
-  private ChessBoard board = new ChessBoard();
-  public Knight(){
-    super(xcor,ycor);
-    isWhite = false;
-  }
-  public Square[] getLegalMoves(){
-    Square[] ans = new Square[8];
-    counter = 0;
-    if (isInsideBounds(xcor+2,ycor+1) /*Is that move within the squares of the board? */
-        && this.isWhite != (board[xcor+2][ycor+1]).isPieceWhite() /*Even if it is legal board wise, is there a piece on that square that is the same color? */){
-      ans[counter] = new Square(xcor+2,ycor+1);
-      counter++;
+
+    private ChessBoard daBoard;
+    private Square[][] board;
+
+    public Knight(int xcor, int ycor, boolean color,ChessBoard daBoard){
+        super(xcor,ycor,color,daBoard);
+        this.daBoard = daBoard;
+        board = daBoard.getBoard();
+
     }
-    //Same thing but for the other seven moves that the Knight could possibly have:
-    if (isInsideBounds(xcor+2,ycor-1)&& this.isWhite != (board[xcor+2][ycor-1]).isPieceWhite()){
-      ans[counter] = new Square(xcor+2,ycor-1);
-      counter++;
+    public Square[] getLegalMoves(){
+        Square[] ans = new Square[8];
+        int counter = 0;
+        if (isInsideBounds(getX()+2,getY()+1) /*Is that move within the squares of the board? */
+                && getColor() != (board[getX()+2][getY()+1]).getPieceType().getColor()
+                        /*Even if it is legal board wise, is there a piece on that square that is the same color? */){
+            ans[counter] = new Square(getX()+2,getY()+1,squareColor(getX(),getY()+1));
+            counter++;
+        }
+        //Same thing but for the other seven moves that the Knight could possibly have:
+        if (isInsideBounds(xcor+2,ycor-1)&& getColor() != (board[xcor+2][ycor-1]).isPieceWhite()){
+            ans[counter] = new Square(xcor+2,ycor-1);
+            counter++;
+        }
+        if (isInsideBounds(xcor+1,ycor-2)&& getColor() != (board[xcor+1][ycor-2]).isPieceWhite()){
+            ans[counter] = new Square(xcor+1,ycor-2);
+            counter++;
+        }
+        if (isInsideBounds(xcor+1,ycor+2)&& getColor() != (board[xcor+1][ycor+2]).isPieceWhite()){
+            ans[counter] = new Square(xcor+1,ycor+2);
+            counter++;
+        }
+        if (isInsideBounds(xcor-2,ycor-1)&& getColor() != (board[xcor-2][ycor-1]).isPieceWhite()){
+            ans[counter] = new Square(xcor-2,ycor-1);
+            counter++;
+        }
+        if (isInsideBounds(xcor-2,ycor+1)&& getColor()!= (board[xcor-2][ycor+1]).isPieceWhite()){
+            ans[counter] = new Square(xcor-2,ycor+1);
+            counter++;
+        }
+        if (isInsideBounds(xcor-1,ycor-2)&& getColor() != (board[xcor-1][ycor-2]).isPieceWhite()){
+            ans[counter] = new Square(xcor-1,ycor-2);
+            counter++;
+        }
+        if (isInsideBounds(xcor-1,ycor+2)&& getColor() != (board[xcor-1][ycor+2]).isPieceWhite()){
+            ans[counter] = new Square(xcor-1,ycor+2);
+            counter++;
+        }
+        return ans;
     }
-    if (isInsideBounds(xcor+1,ycor-2)&& this.isWhite != (board[xcor+1][ycor-2]).isPieceWhite()){
-      ans[counter] = new Square(xcor+1,ycor-2);
-      counter++;
-    }
-    if (isInsideBounds(xcor+1,ycor+2)&& this.isWhite != (board[xcor+1][ycor+2]).isPieceWhite()){
-      ans[counter] = new Square(xcor+1,ycor+2);
-      counter++;
-    }
-    if (isInsideBounds(xcor-2,ycor-1)&& this.isWhite != (board[xcor-2][ycor-1]).isPieceWhite()){
-      ans[counter] = new Square(xcor-2,ycor-1);
-      counter++;
-    }
-    if (isInsideBounds(xcor-2,ycor+1)&& this.isWhite != (board[xcor-2][ycor+1]).isPieceWhite()){
-      ans[counter] = new Square(xcor-2,ycor+1);
-      counter++;
-    }
-    if (isInsideBounds(xcor-1,ycor-2)&& this.isWhite != (board[xcor-1][ycor-2]).isPieceWhite()){
-      ans[counter] = new Square(xcor-1,ycor-2);
-      counter++;
-    }
-    if (isInsideBounds(xcor-1,ycor+2)&& this.isWhite != (board[xcor-1][ycor+2]).isPieceWhite()){
-      ans[counter] = new Square(xcor-1,ycor+2);
-      counter++;
-    }
-    return ans;
-  }
 
 }
